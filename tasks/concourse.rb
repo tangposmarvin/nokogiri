98fd3081fd77210688ff92aafc2f61ae4f0facd2
Mike Dalessio <mike.dalessio@gmail.com>
2020-08-31 09:46:06 -0400

    ci: try using `across` to set up a matrixed pipeline
    and see if it's easier to maintain and/or easier to read

--- tasks/concourse.rb
+++ tasks/concourse.rb
@@ -1,6 +1,7 @@
 require "concourse"
 
-Concourse.new("nokogiri", fly_target: "ci", format: true) do |c|
+Concourse.new("nokogiri", fly_target: "ci", format: true, fly_args_validate_pipeline: "--enable-across-step") do |c|
   c.add_pipeline "nokogiri", "nokogiri.yml"
+  c.add_pipeline "nokogiri-next", "nokogiri-next.yml"
   c.add_pipeline "nokogiri-pr", "nokogiri-pr.yml"
 end
